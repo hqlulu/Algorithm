de89686220c7647dfc2683a2e2d80a2b5fe6b544
hqlulu <hqlulu@gmail.com>
2019-08-11 11:54:06 +0800

    int 打印

--- sort/com/aslibra/sort/AllSortMethod.java
+++ sort/com/aslibra/sort/AllSortMethod.java
@@ -9,19 +9,20 @@ import java.util.Random;
 public class AllSortMethod {
 
     public static void main(String[] args) {
-        final Integer size = 20;
-        final Integer max = 1000;
-        Integer[] toSortArray = new Integer[size];
+        final int size = 20;
+        final int max = 1000;
+        int[] toSortArray = new int[size];
 
         // random
         Random random = new Random();
-        for (Integer i = 0; i < toSortArray.length; i++) {
+        for (int i = 0; i < toSortArray.length; i++) {
             toSortArray[i] = random.nextInt(max);
             System.out.print(toSortArray[i] + ",");
         }
         bubbleSort(toSortArray.clone());
+        insertionSort(toSortArray.clone());
 
-        Integer[] toSortArray2 = new Integer[1];
+        int[] toSortArray2 = new int[1];
         toSortArray2[0] = 1;
         bubbleSort(null);
     }
@@ -31,7 +32,7 @@ public class AllSortMethod {
      * 优化:如果走一遍没有交换,说明已经有序,可以停止
      * @param toSortArray
      */
-    private static void bubbleSort(Integer[] toSortArray) {
+    private static void bubbleSort(int[] toSortArray) {
         System.out.printf("\nbubbleSort");
         if ( toSortArray == null){
             System.out.printf("\narray null");
@@ -41,11 +42,11 @@ public class AllSortMethod {
             printResult(toSortArray);
             return;
         }
-        Integer tmp;
+        int tmp;
         boolean swaped;
         for (Integer i = toSortArray.length - 1; i > 0; i--) {
             swaped = false;
-            for (Integer j = 0; j< i; j++){
+            for (int j = 0; j< i; j++){
                 if (toSortArray[j]>toSortArray[j+1]){
                     // swap
                     tmp = toSortArray[j];
@@ -62,11 +63,45 @@ public class AllSortMethod {
         printResult(toSortArray);
     }
 
+    /**
+     * 取一个数,在已排序区间从后往前找自己的位置,比大小,换位置
+     * 比拟打扑克抓牌过程,先拿起一张,然后抓一张,在手里的牌找位置插入
+     * 优化方式:数据量大时,可以用二分法找位置
+     * @param toSortArray
+     */
+    private static void insertionSort(int[] toSortArray) {
+        System.out.printf("\ninsertionSort");
+        if ( toSortArray == null){
+            System.out.printf("\narray null");
+            return;
+        }
+        if (toSortArray.length <= 1){
+            printResult(toSortArray);
+            return;
+        }
+        int sortedIndex = 0;
+        int tmp;
+        for (int i = sortedIndex+1;i<toSortArray.length;i++){
+            for (int j = sortedIndex + 1;j>0;j--){
+                // 左边的牌大,则换位置
+                if (toSortArray[j]<toSortArray[j-1]){
+                    tmp = toSortArray[j];
+                    toSortArray[j] = toSortArray[j-1];
+                    toSortArray[j-1] = tmp;
+                }
+            }
+            sortedIndex ++;
+        }
+        printResult(toSortArray);
+    }
 
-
-    private static void printResult(Integer[] toSortArray){
+    /**
+     * 打印数组
+     * @param toSortArray
+     */
+    private static void printResult(int[] toSortArray){
         System.out.println("\ntoSortArray = ");
-        for (Integer i = 0; i < toSortArray.length; i++) {
+        for (int i = 0; i < toSortArray.length; i++) {
             System.out.print(toSortArray[i] + ",");
         }
     }
